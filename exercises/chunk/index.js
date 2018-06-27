f1de057f8cb38def4be757b7e04ca5b728e81566
Vladislav Chagaev <chapai@yandex-team.ru>
2018-06-27 14:38:42 +0300

    chunks

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,14 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunks = []
+
+  for (let i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size))
+  }
+
+  return chunks
+}
 
 module.exports = chunk;
