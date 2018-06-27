1c3918f9fd8fc9233f23641f558d14398008cda1
Vladislav Chagaev <chapai@yandex-team.ru>
2018-06-27 15:11:18 +0300

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,8 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  return str.split(' ').map(word => word.replace(word.charAt(0), word.charAt(0).toUpperCase())).join(' ')
+}
 
 module.exports = capitalize;
