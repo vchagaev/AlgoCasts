4cac135e60c4608ebb7016f3ae8a2463435db853
Vladislav Chagaev <chapai@yandex-team.ru>
2018-06-27 14:27:46 +0300

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const mapCharToCount = new Map()
+
+  for (let char of str) {
+    const curCount = mapCharToCount.get(char) || 0
+
+    mapCharToCount.set(char, curCount + 1)
+  }
+
+  let curMaxCharCountPair = [str[0], 1]
+
+  for (let charCountPair of mapCharToCount) {
+    if (charCountPair[1] > curMaxCharCountPair[1]) {
+      curMaxCharCountPair = charCountPair
+    }
+  }
+
+  return curMaxCharCountPair[0]
+}
 
 module.exports = maxChar;
