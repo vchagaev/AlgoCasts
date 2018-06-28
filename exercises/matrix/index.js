d1f98b5e8758fb17e6d1d883e7d77b0bf28400d3
Vladislav Chagaev <chapai@yandex-team.ru>
2018-06-28 11:55:47 +0300

    matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,7 +15,46 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-// TODO: four directions in once
-function matrix(n) {}
+function matrix(n) {
+  const mainMatrix = []
+
+  for (let i = 0; i < n; i++) {
+    mainMatrix.push([])
+  }
+
+  let columnStart = 0
+  let columnEnd = n - 1
+  let rowStart = 0
+  let rowEnd = n - 1
+  let index = 1;
+
+  while (columnStart <= columnEnd && rowStart <= rowEnd) {
+    for (let i = columnStart; i <= columnEnd; i++) {
+      mainMatrix[rowStart][i] = index
+      index++
+    }
+    rowStart++
+
+    for (let i = rowStart; i <= rowEnd; i++) {
+      mainMatrix[i][columnEnd] = index
+      index++
+    }
+    columnEnd--
+
+    for (let i = columnEnd; i >= columnStart; i--) {
+      mainMatrix[rowEnd][i] = index
+      index++
+    }
+    rowEnd--
+
+    for (let i = rowEnd; i >= rowStart; i--) {
+      mainMatrix[i][columnStart] = index
+      index++
+    }
+    columnStart++
+  }
+
+  return mainMatrix
+}
 
 module.exports = matrix;
