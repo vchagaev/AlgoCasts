443aafd625d563256c2b85c83bd891ee62b3007b
Vladislav Chagaev <chapai@yandex-team.ru>
2018-06-29 16:01:20 +0300

    todo sorts

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,14 +1,26 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
+// TODO: sort
 function bubbleSort(arr) {
+  for (let i = 0; i<arr.length; i++) {
+    for (let j = 0; j<arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        const t = arr[i]
+        arr[i] = arr[j]
+        arr[j] = t
+      }
+    }
+  }
 
+  return arr
 }
 
+// TODO: sort
 function selectionSort(arr) {
 
 }
-
+// TODO: sort
 function mergeSort(arr) {
 
 }
