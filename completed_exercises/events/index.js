63960c9371ef06231f23f4a044f4ee4796491db6
Vladislav Chagaev <chapai@yandex-team.ru>
2018-06-29 15:38:29 +0300

    events

--- completed_exercises/events/index.js
+++ completed_exercises/events/index.js
@@ -5,23 +5,23 @@
 
 class Events {
   constructor() {
-    this.events = {};
+    this.eventsToCallbacks = {};
   }
   
   // Register an event handler
   on(eventName, callback) {
-    if (this.events[eventName]) {
-      this.events[eventName].push(callback);
+    if (this.eventsToCallbacks[eventName]) {
+      this.eventsToCallbacks[eventName].push(callback);
     } else {
-      this.events[eventName] = [callback];
+      this.eventsToCallbacks[eventName] = [callback];
     }
   }
 
   // Trigger all callbacks associated
   // with a given eventName
   trigger(eventName) {
-    if (this.events[eventName]) {
-      for (let cb of this.events[eventName]) {
+    if (this.eventsToCallbacks[eventName]) {
+      for (let cb of this.eventsToCallbacks[eventName]) {
         cb();
       }
     }
@@ -30,7 +30,7 @@ class Events {
   // Remove all event handlers associated
   // with the given eventName
   off(eventName) {
-    delete this.events[eventName];
+    delete this.eventsToCallbacks[eventName];
   }
 }
 
--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,38 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.eventsToCallbacks = new Map()
+  }
+
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (!this.eventsToCallbacks.has(eventName)) {
+      this.eventsToCallbacks.set(eventName, [callback])
+    } else {
+      this.eventsToCallbacks.get(eventName).push(callback)
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName, ...args) {
+    if (!this.eventsToCallbacks.has(eventName)) {
+      return
+    }
+
+    this.eventsToCallbacks.get(eventName).forEach((callback) => callback.apply(this, args))
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    if (!this.eventsToCallbacks.has(eventName)) {
+      return
+    }
+
+    this.eventsToCallbacks.delete(eventName)
+  }
 }
 
 module.exports = Events;
