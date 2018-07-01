d0b13c59956c1a3e1dedd8291d81188559f3059b
Vladislav Chagaev <chapai@yandex-team.ru>
2018-07-01 10:10:20 +0300

    level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,9 +11,31 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-// TODO: with special separator
-function levelWidth(root) {
+const LEVEL_SEPARATOR = 'LEVEL_SEPARATOR'
 
+function levelWidth(root) {
+  if (!root) {
+    return null
+  }
+
+  const nodes = [root, LEVEL_SEPARATOR]
+  const levelToCountArray = []
+  let curLevel = 0
+
+  while (nodes.length > 1) {
+    const curNode = nodes.shift()
+
+    if (curNode === LEVEL_SEPARATOR) {
+      curLevel++
+      nodes.push(LEVEL_SEPARATOR)
+    } else {
+      const curLevelCount = levelToCountArray[curLevel] || 0
+      levelToCountArray[curLevel] = curLevelCount + 1
+      nodes.push(...curNode.children)
+    }
+  }
+
+  return levelToCountArray
 }
 
 module.exports = levelWidth;
