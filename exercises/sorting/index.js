f3c7871f368612d3cf20ad03bca1db153a8cad28
Vladislav Chagaev <chapai@yandex-team.ru>
2018-07-01 10:25:39 +0300

    bubble and selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,14 +1,13 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-// TODO: sort
-function bubbleSort(arr) {
-  for (let i = 0; i<arr.length; i++) {
-    for (let j = 0; j<arr.length; j++) {
-      if (arr[i] > arr[j]) {
-        const t = arr[i]
-        arr[i] = arr[j]
-        arr[j] = t
+function bubbleSort (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const t = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = t
       }
     }
   }
@@ -16,17 +15,33 @@ function bubbleSort(arr) {
   return arr
 }
 
+function selectionSort (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let curMinIndex = i
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[curMinIndex] > arr[j]) {
+        curMinIndex = j
+      }
+    }
+
+    if (curMinIndex !== i) {
+      const t = arr[i]
+      arr[i] = arr[curMinIndex]
+      arr[curMinIndex] = t
+    }
+  }
+
+  return arr
+}
+
 // TODO: sort
-function selectionSort(arr) {
-
-}
-// TODO: sort
-function mergeSort(arr) {
+function mergeSort (arr) {
 
 }
 
-function merge(left, right) {
+function merge (left, right) {
 
 }
 
-module.exports = { bubbleSort, selectionSort, mergeSort };
+module.exports = { bubbleSort, selectionSort, mergeSort }
